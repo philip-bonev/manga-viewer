74062bfe7ef08c034566f83ddf506bddb59eaf67
Philip Bonev <philipb@acstre.com>
2026-07-12 19:41:56 +0300

    saves window state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -388,6 +388,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_single_instance::init(|app, argv, _cwd| {
             use tauri::Emitter;
             use tauri::Manager;
